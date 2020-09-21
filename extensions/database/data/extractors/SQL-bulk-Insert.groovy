40559298dd81e1332c9a5ce2457bf3a23d96e955
Hiroaki Maruoka <dm.hiro+git@gmail.com>
2020-09-22 02:28:59 +0900

    Fixed bulk-Insert to work with IntelliJ IDEA Ultimate 2020.2

--- extensions/database/data/extractors/SQL-bulk-Insert.groovy
+++ extensions/database/data/extractors/SQL-bulk-Insert.groovy
@@ -1,44 +1,34 @@
-/*
- * Available context bindings:
- *   COLUMNS     List<DataColumn>
- *   ROWS        Iterable<DataRow>
- *   OUT         { append() }
- *   FORMATTER   { format(row, col); formatValue(Object, col) }
- *   TRANSPOSED  Boolean
- * plus ALL_COLUMNS, TABLE, DIALECT
- *
- * where:
- *   DataRow     { rowNumber(); first(); last(); data(): List<Object>; value(column): Object }
- *   DataColumn  { columnNumber(), name() }
- */
-
 SEP = ", "
 QUOTE     = "\'"
+STRING_PREFIX = DIALECT.getDbms().isMicrosoft() ? "N" : ""
 NEWLINE   = System.getProperty("line.separator")
 
-ROWS_LIST = ROWS.toList()
-ROWS_MAX_IDX = ROWS_LIST.size() - 1
+KEYWORDS_LOWERCASE = com.intellij.database.util.DbSqlUtil.areKeywordsLowerCase(PROJECT)
+KW_INSERT_INTO = KEYWORDS_LOWERCASE ? "insert into " : "INSERT INTO "
+KW_VALUES = KEYWORDS_LOWERCASE ? ") values " : ") VALUES "
+KW_NULL = KEYWORDS_LOWERCASE ? "null" : "NULL"
 
-if (ROWS_MAX_IDX >= 0) {
-    OUT.append("INSERT INTO ")
-    if (TABLE == null) OUT.append("MY_TABLE")
-    else OUT.append(TABLE.getParent().getName()).append(".").append(TABLE.getName())
-    OUT.append(" (")
-
-    COLUMNS.eachWithIndex { column, idx ->
-        OUT.append(column.name()).append(idx != COLUMNS.size() - 1 ? SEP : "")
-    }
-    OUT.append(") VALUES ").append(NEWLINE)
-}
 def record(columns, dataRow, rowIdx) {
     OUT.append("(")
     columns.eachWithIndex { column, idx ->
-        def skipQuote = dataRow.value(column).toString().isNumber() || dataRow.value(column) == null
-        def stringValue = FORMATTER.format(dataRow, column)
-        if (DIALECT.getFamilyId().isMysql()) stringValue = stringValue.replace("\\", "\\\\")
-        OUT.append(skipQuote ? "": QUOTE).append(stringValue.replace(QUOTE, QUOTE + QUOTE))
-           .append(skipQuote ? "": QUOTE).append(idx != columns.size() - 1 ? SEP : "")
+        def value = dataRow.value(column)
+        def skipQuote = value.toString().isNumber() || value == null
+        def stringValue = value != null ? FORMATTER.format(dataRow, column) : KW_NULL
+        if (DIALECT.getDbms().isMysql()) stringValue = stringValue.replace("\\", "\\\\")
+        OUT.append(skipQuote ? "" : (STRING_PREFIX + QUOTE)).append(stringValue.replace(QUOTE, QUOTE + QUOTE))
+                .append(skipQuote ? "" : QUOTE).append(idx != columns.size() - 1 ? SEP : "")
     }
-    OUT.append(")").append(rowIdx != ROWS_MAX_IDX ? SEP : ";" ).append(NEWLINE)
+    OUT.append(")").append(SEP).append(NEWLINE)
 }
-ROWS_LIST.eachWithIndex { row, idx -> record(COLUMNS, row, idx) }
+
+OUT.append(KW_INSERT_INTO)
+if (TABLE == null) OUT.append("MY_TABLE")
+else OUT.append(TABLE.getParent().getName()).append(".").append(TABLE.getName())
+OUT.append(" (")
+
+COLUMNS.eachWithIndex { column, idx ->
+    OUT.append(column.name()).append(idx != COLUMNS.size() - 1 ? SEP : "")
+}
+OUT.append(KW_VALUES).append(NEWLINE)
+
+ROWS.eachWithIndex { row, idx -> record(COLUMNS, row, idx) }
